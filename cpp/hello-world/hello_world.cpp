14feeda5b4633987ae14d0801d6b84a9fde03ee5
David Bruno <davidbruno2000@gmail.com>
2019-02-20 01:53:26 -0500

    initial commit

---
+++ cpp/hello-world/hello_world.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Hello_World.cpp
+// Author      : David Bruno
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++,
+//============================================================================
+#include "hello_world.h"
+
+using namespace std;
+
+namespace hello_world
+{
+	
+string hello()
+{
+    return "Hello, World!";
+}
+
+}
